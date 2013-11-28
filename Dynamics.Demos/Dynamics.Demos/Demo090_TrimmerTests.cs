e97d176ce58d1d748eef97ca212bd5c4e3196fb0
Tim Bourguignon <timothepdotnet@gmail.com>
2013-11-28 16:39:17 +0100

    Trimmer structural change

--- Dynamics.Demos/Dynamics.Demos/Demo090_TrimmerTests.cs
+++ Dynamics.Demos/Dynamics.Demos/Demo090_TrimmerTests.cs
@@ -5,17 +5,18 @@
     [TestFixture]
     public class TestsDemo09
     {
-        private const string PathToTrim = @"C:\Dev\Ruby193\bin\ruby.exe";
         private readonly dynamic trimmer = new Trimmer();
         private readonly dynamic easyTrimmer = new EasyTrimmer();
 
-        // EASY CASE, ONE LEVEL
+        private const string PathToTrim = @"C:\Dev\Ruby193\bin\ruby.exe";
+
+        // EASY CASE, EASY-TRIMMER
 
         [Test]
-        public void TestDemo09_EasyTrimmer_ExtractDriveLetter()
+        public void TestDemo09_EasyTrimmer_ExtractFileName()
         {
-            string driveLetter = easyTrimmer.ExtractDriveLetter(PathToTrim);
-            Assert.AreEqual("C", driveLetter);
+            string fileName = easyTrimmer.ExtractFileName(PathToTrim);
+            Assert.AreEqual("ruby.exe", fileName);
         }
 
         [Test]
@@ -26,47 +27,54 @@
             Assert.AreEqual("ruby", fileNameWithoutExtension);
         }
 
-        // ELABORATE CASE
+        // ELABORATE CASE - TRIMMER
+
+        [Test]
+        public void TestDemo09ExtractDrive()
+        {
+            Wrapper drive = trimmer.ExtractDrive(PathToTrim);
+            Assert.AreEqual(@"C:\", drive.Value);
+        }
 
         [Test]
         public void TestDemo09ExtractDriveLetter()
         {
-            DynamicWrapper driveLetter = trimmer.ExtractDriveLetter(PathToTrim);
+            Wrapper driveLetter = trimmer.ExtractDrive(PathToTrim).LetterOnly();
             Assert.AreEqual("C", driveLetter.Value);
         }
 
         [Test]
         public void TestDemo09ExtractFileExtension()
         {
-            DynamicWrapper fileExtension = trimmer.ExtractFileExtension(PathToTrim);
+            Wrapper fileExtension = trimmer.ExtractFileExtension(PathToTrim);
             Assert.AreEqual(".exe", fileExtension.Value);
         }
 
         [Test]
         public void TestDemo09ExtractFileName()
         {
-            DynamicWrapper fileName = trimmer.ExtractFileName(PathToTrim);
+            Wrapper fileName = trimmer.ExtractFileName(PathToTrim);
             Assert.AreEqual("ruby.exe", fileName.Value);
         }
 
         [Test]
         public void TestDemo09ExtractFileNameWithoutExtension()
         {
-            DynamicWrapper fileNameWithoutExtension = trimmer.ExtractFileName(PathToTrim).WithoutExtension();
+            Wrapper fileNameWithoutExtension = trimmer.ExtractFileName(PathToTrim).WithoutExtension();
             Assert.AreEqual("ruby", fileNameWithoutExtension.Value);
         }
 
         [Test]
         public void TestDemo09ExtractPath()
         {
-            DynamicWrapper path = trimmer.ExtractPath(PathToTrim);
+            Wrapper path = trimmer.ExtractPath(PathToTrim);
             Assert.AreEqual(@"C:\Dev\Ruby193\bin", path.Value);
         }
 
         [Test]
         public void TestDemo09ExtractPathRoot()
         {
-            DynamicWrapper pathRoot = trimmer.ExtractPathRoot(PathToTrim);
+            Wrapper pathRoot = trimmer.ExtractPathRoot(PathToTrim);
             Assert.AreEqual(@"C:\", pathRoot.Value);
         }
     }
--- Dynamics.Demos/Dynamics.Demos/Demo091_Trimmer.cs
+++ Dynamics.Demos/Dynamics.Demos/Demo091_Trimmer.cs
@@ -5,21 +5,6 @@ using System.IO;
 
 namespace Dynamics.Demos
 {
-    internal class Trimmer : DynamicObject
-    {
-        /// <summary>
-        ///   Function handling the first call on the dynamic object
-        /// </summary>
-        /// <param name="binder">A wrapper object containing the name of the member that was called "ExtractSomething"</param>
-        /// <param name="result">An initialized DynamicWrapper on which to invoke the method</param>
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
-        {
-            //Create a Wrapper and save the member name that was called "ExtractSomething.."
-            result = new DynamicWrapper(binder.Name);
-            return true;
-        }
-    }
-
     internal class EasyTrimmer : DynamicObject
     {
         public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
@@ -29,11 +14,6 @@ namespace Dynamics.Demos
             {
                 workingMember = workingMember.Remove(0, "Extract".Length);
 
-                if (workingMember.StartsWith("DriveLetter"))
-                {
-                    result = (args[0].ToString())[0].ToString(CultureInfo.InvariantCulture);
-                    return true;
-                }
                 if (workingMember.StartsWith("FileName"))
                 {
                     result = Path.GetFileName(args[0].ToString());
@@ -45,84 +25,62 @@ namespace Dynamics.Demos
             return false;
         }
     }
-
-    internal class DynamicWrapper : DynamicObject
+    
+    internal class Trimmer : DynamicObject
     {
-        private readonly string member;
-        public string Value;
-
-        /// <summary>
-        ///   Constructor
-        /// </summary>
-        /// <param name="memberName">The name of the member called to come here (ExtractSomething)</param>
-        /// <param name="result">The result of the parsing sofar (in case of chaining)</param>
-        public DynamicWrapper(string memberName, string result = null)
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
         {
-            member = memberName;
-            Value = result;
+            result = Parser.Parse(binder.Name, args[0].ToString());
+            return true;
         }
+    }
 
-        /// <summary>
-        ///   Handler for the commands
-        /// </summary>
-        /// <param name="binder">N/A</param>
-        /// <param name="args">Contains the eventual parameters</param>
-        /// <param name="result">Contains a DynamicWrapper object representing the state of the parsing</param>
-        public override bool TryInvoke(InvokeBinder binder, object[] args, out object result)
+    internal static class Parser
+    {
+        public static Wrapper Parse(string how, string what)
         {
-            var workingMember = member;
-            if (workingMember.StartsWith("Extract"))
+            if (how.StartsWith("Extract"))
             {
-                workingMember = workingMember.Remove(0, "Extract".Length);
+                how = how.Remove(0, "Extract".Length);
 
-                if (workingMember.StartsWith("DriveLetter"))
-                {
-                    result = new DynamicWrapper(member, (args[0].ToString())[0].ToString(CultureInfo.InvariantCulture));
-                    return true;
-                }
-                if (workingMember.StartsWith("PathRoot"))
-                {
-                    result = new DynamicWrapper(member, Path.GetPathRoot(args[0].ToString()));
-                    return true;
-                }
-                if (workingMember.StartsWith("Path"))
-                {
-                    result = new DynamicWrapper(member, Path.GetDirectoryName(args[0].ToString()));
-                    return true;
-                }
-                if (workingMember.StartsWith("FileExtension"))
-                {
-                    result = new DynamicWrapper(member, Path.GetExtension(args[0].ToString()));
-                    return true;
-                }
-                if (workingMember.StartsWith("FileName"))
-                {
-                    result = new DynamicWrapper(member, Path.GetFileName(args[0].ToString()));
-                    return true;
-                }
+                if (how.StartsWith("Drive"))
+                    return new Wrapper(what.Substring(0, 3));
+                if (how.StartsWith("PathRoot"))
+                    return new Wrapper(Path.GetPathRoot(what));
+                if (how.StartsWith("Path"))
+                    return new Wrapper(Path.GetDirectoryName(what));
+                if (how.StartsWith("FileExtension"))
+                    return new Wrapper(Path.GetExtension(what));
+                if (how.StartsWith("FileName"))
+                    return new Wrapper(Path.GetFileName(what));
             }
 
-            if (workingMember.StartsWith("Without"))
+            if (how.StartsWith("Without"))
             {
-                workingMember = workingMember.Remove(0, "Without".Length);
-                if (workingMember.StartsWith("Extension"))
-                {
-                    result = new DynamicWrapper(member, Value.Remove(Value.LastIndexOf('.')));
-                    return true;
-                }
+                how = how.Remove(0, "Without".Length);
+                if (how.StartsWith("Extension"))
+                    return new Wrapper(what.Remove(what.LastIndexOf('.')));
             }
+
+            if (how.StartsWith("LetterOnly"))
+                return new Wrapper(what[0].ToString(CultureInfo.InvariantCulture));
 
             throw new Exception("Parsing failed");
         }
+    }
+    
+    internal class Wrapper: DynamicObject
+    {
+        public string Value { get; set; }
 
-        /// <summary>
-        ///   Function handling the subsequent/chained calls to dynamic objects
-        /// </summary>
-        /// <param name="binder">A wrapper object containing the name of the member that was called "WithoutExtension"</param>
-        /// <param name="result">An initialized DynamicWrapper on which to invoke the method</param>
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        public Wrapper(string value)
         {
-            result = new DynamicWrapper(binder.Name, Value);
+            Value = value;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = Parser.Parse(binder.Name, this.Value);
             return true;
         }
     }
